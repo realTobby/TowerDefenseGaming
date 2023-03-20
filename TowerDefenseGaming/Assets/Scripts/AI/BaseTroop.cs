8778072dff2a636493140113de86539dfde789dd
realTobby <tkattanek00@gmail.com>
2023-03-20 20:17:38 +0100

    Troop Movement
    Waypoint System, Troop Movement

---
+++ TowerDefenseGaming/Assets/Scripts/AI/BaseTroop.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DefaultExecutionOrder(5)]
+public class BaseTroop : MonoBehaviour
+{
+    public float Speed = 5f;
+
+    int waypointIndex = 1;
+    Vector3 currentTargetPos;
+
+    private void GetNextWaypoint()
+    {
+        currentTargetPos = GameManager.Instance.GetWaypoint(waypointIndex);
+        waypointIndex++;
+
+        if(currentTargetPos.x == 666 && currentTargetPos.y == 666 && currentTargetPos.z == 666) // Pretty evil code if you know what it does. Cruel even. Caused by a hate for non-nullable values :)
+        {
+            Suicide();
+        }
+    }
+
+    private void Suicide()
+    {
+        Destroy(this.gameObject);
+    }
+
+
+    IEnumerator Move()
+    {
+        float time = 0;
+        Vector3 startPosition = this.transform.position;
+        if (startPosition == currentTargetPos) yield return null;
+        while (AmIAtTarget(this.transform.position, currentTargetPos) == false)
+        {
+            transform.position = Vector3.Lerp(startPosition, currentTargetPos, time);
+            time += Time.deltaTime * Speed;
+            yield return null;
+        }
+
+        GetNextWaypoint();
+        StartCoroutine(nameof(Move));
+    }
+
+    private bool AmIAtTarget(Vector3 a, Vector3 b)
+    {
+        if(a.x == b.x && a.z == b.z) // because in our game, the troops dont actually care about their y-dimension :)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetNextWaypoint();
+        StartCoroutine(nameof(Move));
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- TowerDefenseGaming/Assets/Scripts/Managers/GameManager.cs
+++ TowerDefenseGaming/Assets/Scripts/Managers/GameManager.cs
@@ -2,8 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[DefaultExecutionOrder(0)]
 public class GameManager : MonoBehaviour
 {
+    private static GameManager _instance = null;
+    public static GameManager Instance => _instance;
+
     public GameObject WayPointsRoot;
 
     public GameObject Troop;
@@ -12,6 +16,21 @@ public class GameManager : MonoBehaviour
 
     private Vector3 CurrentGoalPosition;
     private int CurrentWayPointIndex = 0;
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    public Vector3 GetWaypoint(int index)
+    {
+        if(index < WayPointsArray.Length)
+        {
+            return WayPointsArray[index].transform.position;
+        }
+        return new Vector3(666,666,666);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,8 +39,6 @@ public class GameManager : MonoBehaviour
         // Wie könnte man die "Troop" Instanz an den WayPoints langbewegen?
 
     }
-
-
 
     private void LoadWayPoints()
     {
@@ -32,7 +49,6 @@ public class GameManager : MonoBehaviour
             WayPointsArray[i] = WayPointsRoot.transform.GetChild(i).gameObject;
         }
     }
-
 
     // Update is called once per frame
     void Update()
